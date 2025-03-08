5cc1bbe7726800e5262167977b26b2b8e86ed4cf
Secretus1204 <ryletabay04@gmail.com>
2025-03-08 08:18:55 +0800

    Update DBConnection.php

--- SQL/config/DBConnection.php
+++ SQL/config/DBConnection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "yaphub";
+$servername = "sql301.infinityfree.com";
+$username = "if0_38467642";
+$password = "9C8XtLxQWgEyE0h";
+$db_name = "if0_38467642_yaphub";
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$db_name;charset=utf8mb4", $username, $password);
